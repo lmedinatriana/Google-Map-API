506ace1633826e94f6c551ad46ccbaf83e42f40a
lmedinatriana <miguemetri@gmail.com>
2017-12-01 17:16:57 -0500

    Added side menu

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-var BetterListModel = function () {
-    this.itemToAdd = ko.observable("");
-    this.allItems = ko.observableArray(["Fries", "Eggs Benedict", "Ham", "Cheese"]); // Initial items
-    this.selectedItems = ko.observableArray(["Ham"]);                                // Initial selection
- 
-    this.addItem = function () {
-        if ((this.itemToAdd() != "") && (this.allItems.indexOf(this.itemToAdd()) < 0)) // Prevent blanks and duplicates
-            this.allItems.push(this.itemToAdd());
-        this.itemToAdd(""); // Clear the text box
-    };
- 
-    this.removeSelected = function () {
-        this.allItems.removeAll(this.selectedItems());
-        this.selectedItems([]); // Clear selection
-    };
- 
-    this.sortItems = function() {
-        this.allItems.sort();
-    };
-};
- 
-ko.applyBindings(new BetterListModel());
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
